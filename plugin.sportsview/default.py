c8886141940b47670967493ec88964b6fdaeeb7c
ZappBrannigannnn <136344138+ZappBrannigannnn@users.noreply.github.com>
2023-09-13 12:48:46 -0400

    Add files via upload

---
+++ plugin.sportsview/default.py
@@ -0,0 +1,17 @@
+import xbmcaddon
+from window_manager import WindowManager
+
+class MyAddon(xbmcaddon.Addon):
+    def __init__(self):
+        xbmcaddon.Addon.__init__(self)
+        self.window_manager = WindowManager()
+
+    def defaultrun(self):
+        print("Running MyAddon")
+        # Run the landing page window
+        self.window_manager.show_landing_page()
+
+#initialize instance of MyAddon as addon 
+addon = MyAddon()
+
+addon.defaultrun()
